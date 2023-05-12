f7539c9db8b1d9cc6495da9f07469365315097ba
yellowyears <yellowyears200@gmail.com>
2023-05-12 21:29:32 +0100

    Initial Commit

---
+++ DeveloperTools/Main.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+
+namespace DeveloperTools
+{
+    public static class BuildInfo
+    {
+        public const string Name = "DeveloperTools"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "yellowyears"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    internal class Main : MelonMod
+    {
+        
+    }
+    
+}
---
+++ DeveloperTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(DeveloperTools.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(DeveloperTools.BuildInfo.Company)]
+[assembly: AssemblyProduct(DeveloperTools.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + DeveloperTools.BuildInfo.Author)]
+[assembly: AssemblyTrademark(DeveloperTools.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(DeveloperTools.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(DeveloperTools.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(DeveloperTools.Main), DeveloperTools.BuildInfo.Name, DeveloperTools.BuildInfo.Version, DeveloperTools.BuildInfo.Author, DeveloperTools.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
