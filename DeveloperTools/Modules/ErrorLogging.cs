aca3b1994229ea39d94514264edcd172e66e6292
yellowyears <yellowyears200@gmail.com>
2023-05-14 19:57:16 +0100

    Add other logging patches

--- DeveloperTools/Modules/ErrorLogging.cs
+++ DeveloperTools/Modules/ErrorLogging.cs
@@ -1,7 +1,6 @@
 ﻿using HarmonyLib;
 using MelonLoader;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace DeveloperTools.Modules
 {
@@ -50,10 +49,34 @@ namespace DeveloperTools.Modules
         private static void PatchLogMethods(HarmonyLib.Harmony instance)
         {
             // Manually patch the methods so they are not patched despite createErrorLog preference being false
+            
+            // Debug.Log
             instance.Patch(typeof(Debug).GetMethod("Log", new [] { typeof(Il2CppSystem.Object) }), 
                 postfix: new HarmonyMethod(typeof(LogPatch).GetMethod("Postfix")));
             instance.Patch(typeof(Debug).GetMethod("Log", new[] { typeof(Il2CppSystem.Object), typeof(Object) }), 
                 postfix: new HarmonyMethod(typeof(LogContextPatch).GetMethod("Postfix")));
+            
+            // Debug.LogWarning
+            instance.Patch(typeof(Debug).GetMethod("LogWarning", new[] { typeof(Il2CppSystem.Object) }),
+                postfix: new HarmonyMethod(typeof(LogWarningPatch).GetMethod("Postfix")));
+            instance.Patch(typeof(Debug).GetMethod("LogWarning", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
+                postfix: new HarmonyMethod(typeof(LogWarningContextPatch).GetMethod("Postfix")));
+            
+            // Debug.LogError
+            instance.Patch(typeof(Debug).GetMethod("LogError", new[] { typeof(Il2CppSystem.Object) }),
+                postfix: new HarmonyMethod(typeof(LogErrorPatch).GetMethod("Postfix")));
+            instance.Patch(typeof(Debug).GetMethod("LogError", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
+                postfix: new HarmonyMethod(typeof(LogErrorContextPatch).GetMethod("Postfix")));
+            
+            // Debug.LogException
+            instance.Patch(typeof(Debug).GetMethod("LogException", new[] { typeof(Il2CppSystem.Object) }),
+                postfix: new HarmonyMethod(typeof(LogExceptionPatch).GetMethod("Postfix")));
+            instance.Patch(typeof(Debug).GetMethod("LogException", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
+                postfix: new HarmonyMethod(typeof(LogExceptionContextPatch).GetMethod("Postfix")));
+            
+            // Debug.LogAssertion
+            instance.Patch(typeof(Debug).GetMethod("LogAssertion", new[] { typeof(Il2CppSystem.Object) }),
+                postfix: new HarmonyMethod(typeof(LogAssertionPatch).GetMethod("Postfix")));
         }
         
         private static class LogPatch
@@ -71,5 +94,66 @@ namespace DeveloperTools.Modules
                 WriteLog($"{message} {context}\n");
             }
         }
+
+        
+        private static class LogWarningPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message)
+            {
+                WriteLog($"{message}\n");
+            }
+        }
+
+        private static class LogWarningContextPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message, Object context)
+            {
+                WriteLog($"{message} {context}\n");
+            }
+        }
+
+        
+        private static class LogErrorPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message)
+            {
+                WriteLog($"{message}");
+            }
+        }     
+        
+        private static class LogErrorContextPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message, Object context)
+            {
+                WriteLog($"{message} {context}");
+            }
+        }
+
+        
+        private static class LogExceptionPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message)
+            {
+                WriteLog($"{message}");
+            }
+        }
+        
+        private static class LogExceptionContextPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message, Object context)
+            {
+                WriteLog($"{message} {context}");
+            }
+        }
+        
+        
+        private static class LogAssertionPatch
+        {
+            public static void Postfix(Il2CppSystem.Object message)
+            {
+                WriteLog($"{message}");
+            }
+        }
+        
     }
 }
