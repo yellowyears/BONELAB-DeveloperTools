2557b186548b9726d39ee162fdb047cf1e1ce2b4
yellowyears <yellowyears200@gmail.com>
2023-05-14 20:49:14 +0100

    Add prev log and remove error log preference

--- DeveloperTools/Modules/ErrorLogging.cs
+++ DeveloperTools/Modules/ErrorLogging.cs
@@ -1,13 +1,15 @@
 ﻿using HarmonyLib;
 using MelonLoader;
+using UnhollowerBaseLib;
 using UnityEngine;
+using System.IO;
 
 namespace DeveloperTools.Modules
 {
     internal class ErrorLogging
     {
 
-        private static string _logDirectory = null;
+        private static string _logDirectory = MelonUtils.MelonLoaderDirectory + @"\ErrorLog.txt";
         private static readonly string _melonLoaderWarning = "-------------------------------------------------------" +
                                                              "------------------------------------------------\n~   " +
                                                             "This Game has been MODIFIED using MelonLoader. DO NOT " +
@@ -17,57 +19,68 @@ namespace DeveloperTools.Modules
         
         public static void SetupLogging(HarmonyLib.Harmony instance)
         {
-            // TODO: Improve validation here
-            if (Preferences.errorLogDirectory.Value != null)
-            {
-                if (Preferences.errorLogDirectory.Value.StartsWith(@"\"))
-                {
-                    _logDirectory = MelonUtils.MelonLoaderDirectory + Preferences.errorLogDirectory.Value;
-                }
-                else
-                {
-                    _logDirectory = MelonUtils.MelonLoaderDirectory + @"\" + Preferences.errorLogDirectory.Value;
-                }
-            }
-            else
-            {
-                _logDirectory = MelonUtils.MelonLoaderDirectory + @"\ErrorLog.txt";
-            }
+            // Check for a previous log and renames it to ErrorLog-prev.txt
+            CheckForLog();
             
             // Write the ML warning first to the text file
             // This has to be done as the melon will be initialised after the original ML warning has been logged
-            System.IO.File.WriteAllText(_logDirectory, _melonLoaderWarning);
+            File.WriteAllText(_logDirectory, _melonLoaderWarning);
             
+            // Manually patch the methods so they are not patched when Preferences.createErrorLog is false
             PatchLogMethods(instance);
+        }
+
+        private static void CheckForLog()
+        {
+            // Check for any existing log before proceeding
+            if (!File.Exists(_logDirectory)) return;
+
+            // Create the -prev filepath from the _logDirectory
+            var prevLogPath = MelonUtils.MelonLoaderDirectory + @"\ErrorLog-prev.txt";
+            
+            // Copy the existing log into the new prev log
+            File.Copy(_logDirectory, prevLogPath);
+            
+            // Delete the original log
+            File.Delete(_logDirectory);
         }
         
         private static void WriteLog(string message)
         {
-            System.IO.File.AppendAllText(_logDirectory, message);
+            // Validate whether or not the message ends with a newline to prevent the logs from being on the same line
+            if (!message.EndsWith("\n")) message += "\n";
+            
+            File.AppendAllText(_logDirectory, message);
         }
-        
+
         private static void PatchLogMethods(HarmonyLib.Harmony instance)
         {
-            // Manually patch the methods so they are not patched despite createErrorLog preference being false
-            
             // Debug.Log
-            instance.Patch(typeof(Debug).GetMethod("Log", new [] { typeof(Il2CppSystem.Object) }), 
+            instance.Patch(typeof(Debug).GetMethod("Log", new[] { typeof(Il2CppSystem.Object) }),
                 postfix: new HarmonyMethod(typeof(LogPatch).GetMethod("Postfix")));
-            instance.Patch(typeof(Debug).GetMethod("Log", new[] { typeof(Il2CppSystem.Object), typeof(Object) }), 
+            instance.Patch(typeof(Debug).GetMethod("Log", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
                 postfix: new HarmonyMethod(typeof(LogContextPatch).GetMethod("Postfix")));
-            
+            instance.Patch(typeof(Debug).GetMethod("LogFormat", new[] { typeof(string), typeof(Il2CppReferenceArray<Il2CppSystem.Object>) }),
+                postfix: new HarmonyMethod(typeof(LogFormatPatch).GetMethod("Postfix")));
+
             // Debug.LogWarning
             instance.Patch(typeof(Debug).GetMethod("LogWarning", new[] { typeof(Il2CppSystem.Object) }),
                 postfix: new HarmonyMethod(typeof(LogWarningPatch).GetMethod("Postfix")));
             instance.Patch(typeof(Debug).GetMethod("LogWarning", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
                 postfix: new HarmonyMethod(typeof(LogWarningContextPatch).GetMethod("Postfix")));
-            
+            instance.Patch(typeof(Debug).GetMethod("LogWarningFormat", new[] { typeof(string), typeof(Il2CppReferenceArray<Il2CppSystem.Object>) }),
+                postfix: new HarmonyMethod(typeof(LogWarningFormatPatch).GetMethod("Postfix")));
+
             // Debug.LogError
             instance.Patch(typeof(Debug).GetMethod("LogError", new[] { typeof(Il2CppSystem.Object) }),
                 postfix: new HarmonyMethod(typeof(LogErrorPatch).GetMethod("Postfix")));
             instance.Patch(typeof(Debug).GetMethod("LogError", new[] { typeof(Il2CppSystem.Object), typeof(Object) }),
                 postfix: new HarmonyMethod(typeof(LogErrorContextPatch).GetMethod("Postfix")));
-            
+            instance.Patch(typeof(Debug).GetMethod("LogErrorFormat", new[] { typeof(string), typeof(Il2CppReferenceArray<Il2CppSystem.Object>) }),
+                postfix: new HarmonyMethod(typeof(LogErrorFormatPatch).GetMethod("Postfix")));
+            instance.Patch(typeof(Debug).GetMethod("LogErrorFormat", new[] { typeof(Object), typeof(string), typeof(Il2CppReferenceArray<Il2CppSystem.Object>) }),
+                postfix: new HarmonyMethod(typeof(LogErrorFormatContextPatch).GetMethod("Postfix")));
+
             // Debug.LogException
             instance.Patch(typeof(Debug).GetMethod("LogException", new[] { typeof(Il2CppSystem.Object) }),
                 postfix: new HarmonyMethod(typeof(LogExceptionPatch).GetMethod("Postfix")));
@@ -77,13 +90,17 @@ namespace DeveloperTools.Modules
             // Debug.LogAssertion
             instance.Patch(typeof(Debug).GetMethod("LogAssertion", new[] { typeof(Il2CppSystem.Object) }),
                 postfix: new HarmonyMethod(typeof(LogAssertionPatch).GetMethod("Postfix")));
+            instance.Patch(typeof(Debug).GetMethod("LogAssertionFormat", new[] { typeof(string), typeof(Il2CppReferenceArray<Il2CppSystem.Object>) }),
+                postfix: new HarmonyMethod(typeof(LogAssertionFormatPatch).GetMethod("Postfix")));
         }
+        
+        // Debug.Log
         
         private static class LogPatch
         {
             public static void Postfix(Il2CppSystem.Object message)
             {
-                WriteLog($"{message}\n");
+                WriteLog($"{message}");
             }
         }        
         
@@ -91,16 +108,25 @@ namespace DeveloperTools.Modules
         {
             public static void Postfix(Il2CppSystem.Object message, Object context)
             {
-                WriteLog($"{message} {context}\n");
+                WriteLog($"{message} {context}");
             }
         }
 
+        private static class LogFormatPatch
+        {
+            public static void Postfix(string format, Il2CppReferenceArray<Il2CppSystem.Object> args)
+            {
+                WriteLog($"{format}");
+            }
+        }
+
+        // Debug.LogWarning
         
         private static class LogWarningPatch
         {
             public static void Postfix(Il2CppSystem.Object message)
             {
-                WriteLog($"{message}\n");
+                WriteLog($"{message}");
             }
         }
 
@@ -108,10 +134,19 @@ namespace DeveloperTools.Modules
         {
             public static void Postfix(Il2CppSystem.Object message, Object context)
             {
-                WriteLog($"{message} {context}\n");
+                WriteLog($"{message} {context}");
             }
         }
 
+        private static class LogWarningFormatPatch
+        {
+            public static void Postfix(string format, Il2CppReferenceArray<Il2CppSystem.Object> args)
+            {
+                WriteLog(format);
+            }
+        }
+        
+        // Debug.LogError
         
         private static class LogErrorPatch
         {
@@ -129,6 +164,23 @@ namespace DeveloperTools.Modules
             }
         }
 
+        private static class LogErrorFormatPatch
+        {
+            public static void Postfix(string format, Il2CppReferenceArray<Il2CppSystem.Object> args)
+            {
+                WriteLog(format);
+            }
+        }
+
+        private static class LogErrorFormatContextPatch
+        {
+            public static void Postfix(Object context, string format, Il2CppReferenceArray<Il2CppSystem.Object> args)
+            {
+                WriteLog($"{format} {context}");
+            }
+        }
+        
+        // Debug.LogException
         
         private static class LogExceptionPatch
         {
@@ -145,13 +197,22 @@ namespace DeveloperTools.Modules
                 WriteLog($"{message} {context}");
             }
         }
-        
+
+        // Debug.LogAssertion
         
         private static class LogAssertionPatch
         {
             public static void Postfix(Il2CppSystem.Object message)
             {
                 WriteLog($"{message}");
+            }
+        }
+
+        private static class LogAssertionFormatPatch
+        {
+            public static void Postfix(string format, Il2CppReferenceArray<Il2CppSystem.Object> args)
+            {
+                WriteLog(format);
             }
         }
         
